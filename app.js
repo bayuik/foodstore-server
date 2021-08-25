b9c67d3e779c3c272ae41ce766b9399a532f13d9
bayuik <bayuindrakusuma05.com>
2021-08-25 12:49:14 +0700

    add categories

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const productRouter = require('./app/product/router')
+const productRouter = require("./app/product/router");
 var app = express();
-
+const categoryRouter = require("./app/category/router");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", productRouter);
+app.use("/api", categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app/product/model.js
+++ app/product/model.js
@@ -5,7 +5,7 @@ const productSchema = Schema(
   {
     name: {
       type: String,
-      mingLength: [3, "Panjang nama makanan minimal 3 karakter"],
+      minLength: [3, "Panjang nama makanan minimal 3 karakter"],
       maxLength: [255, "Panjang nama makanan maksimal 255 karakter"],
       required: [true, "Nama produk harus diisi"],
     },
