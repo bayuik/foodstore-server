b068787697b483f4e4daea8646a1ac25320a5d35
bayuik <bayuindrakusuma05.com>
2021-08-25 07:44:53 +0700

    add get and update product

--- app/product/controller.js
+++ app/product/controller.js
@@ -13,7 +13,7 @@ async function store(req, res, next) {
       let target_path = path.resolve(config.rootPath, `public/upload/${filename}`);
       const src = fs.createReadStream(tmp_path);
       const dest = fs.createWriteStream(target_path);
-      src.pipe(dest)
+      src.pipe(dest);
       src.on("end", async () => {
         try {
           let product = new Product({ ...payload, image_url: filename });
@@ -31,9 +31,9 @@ async function store(req, res, next) {
           next(err);
         }
       });
-      src.on('error', async() => {
-        next(err)
-      })
+      src.on("error", async () => {
+        next(err);
+      });
     } else {
       let product = new Product(payload);
       await product.save();
@@ -51,6 +51,72 @@ async function store(req, res, next) {
   }
 }
 
+async function index(req, res, next) {
+  try {
+    let { limit = 10, skip = 0 } = req.query;
+    let products = await Product.find().limit(parseInt(limit)).skip(parseInt(skip));
+    return res.json(products);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function update(req, res, next) {
+  try {
+    let payload = req.body;
+    if (req.file) {
+      let tmp_path = req.file.path;
+      let originalExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+      let filename = req.file.filename + "." + originalExt;
+      let target_path = path.resolve(config.rootPath, `public/upload/${filename}`);
+      const src = fs.createReadStream(tmp_path);
+      const dest = fs.createWriteStream(target_path);
+      src.pipe(dest);
+      src.on("end", async () => {
+        try {
+          let product = await Product.findOne({ _id: req.params.id });
+          let currentImage = `${config.rootPath}/public/upload/${product.image_url}`;
+          if (fs.existsSync(currentImage)) {
+            fs.unlinkSync(currentImage);
+          }
+          product = await Product.findOneAndUpdate({ _id: req.params.id }, { ...payload, image_url: filename }, { new: true, runValidators: true });
+          return res.json(product);
+        } catch (err) {
+          // ----- cek tipe error ---- //
+          if (err && err.name === "ValidationError") {
+            return res.json({
+              error: 1,
+              message: err.message,
+              fields: err.errors,
+            });
+          }
+          next(err);
+        }
+      });
+      src.on("error", async () => {
+        next(err);
+      });
+    } else {
+      // (6) update produk jika tidak ada file upload
+      let product = await Product.findOneAndUpdate({ _id: req.params.id }, payload, { new: true, runValidators: true });
+      return res.json(product);
+    }
+  } catch (err) {
+    // ----- cek tipe error ---- //
+    if (err && err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
+
+    next(err);
+  }
+}
+
 module.exports = {
+  index,
+  update,
   store,
 };
--- app/product/router.js
+++ app/product/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const os = require("os");
 const multer = require("multer");
+const os = require("os");
 const productController = require("./controller");
+router.get("/products", productController.index);
 router.post("/products", multer({ dest: os.tmpdir() }).single("image"), productController.store);
+router.put("/products/:id", multer({ dest: os.tmpdir() }).single("image"), productController.update);
 module.exports = router;
