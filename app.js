e79bf1446432abb1e23172050f35af0e71d59ff1
bayuik <bayuindrakusuma05.com>
2021-08-25 14:38:33 +0700

    add tag controller

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 const productRouter = require("./app/product/router");
 var app = express();
 const categoryRouter = require("./app/category/router");
-
+const tagRouter = require("./app/tag/router");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", productRouter);
 app.use("/api", categoryRouter);
+app.use("/api", tagRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
